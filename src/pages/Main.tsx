f994217e008cb606713441c66f252d84a4bf9a48
Hamin Lee <whereanna7@gmail.com>
2023-12-27 19:17:52 +0900

    [add] Header, SearchEngine added

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,13 +2,19 @@ import Header from "@/components/main/Header";
 import MainPicLists from "@/components/main/MainPicLists";
 import SearchEngine from "@/components/main/SearchEngine";
 import React from "react";
+import { styled } from "styled-components";
 const Main = () => {
   return (
-    <div>
+    <StMainContainer>
       <Header />
       <SearchEngine />
       <MainPicLists />
-    </div>
+    </StMainContainer>
   );
 };
 export default Main;
+
+const StMainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
