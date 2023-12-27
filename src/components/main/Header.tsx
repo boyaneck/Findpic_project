bf33513e6741ee7220994f63122299b9844f38a1
Hamin Lee <whereanna7@gmail.com>
2023-12-27 17:43:52 +0900

    [add] firebase mockdata 가져오기

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import { db } from "@/common/firebase_hm";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect } from "react";
 
 const Header = () => {
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const sampleCollection = collection(db, "findpicLists");
+        const Snapshot = await getDocs(sampleCollection);
+        return Snapshot.docs;
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetch().then((res) =>
+      console.log(
+        "res",
+        res?.map((list) => list.data())
+      )
+    );
+  }, []);
   return <div>Header</div>;
 };
 
--- src/pages/api/findpicLists.ts
+++ src/pages/api/findpicLists.ts
@@ -1,12 +1,22 @@
-// import { useEffect } from "react";
-// import { Firestore, doc, getDoc } from "firebase/firestore";
-// import { db } from "@/common/firebase";
+// import { db } from "@/common/firebase_hm";
+// import { collection, addDoc, doc, getDoc } from "firebase/firestore";
 
-// const fetchData = async () => {
-//   const docRef = doc(db, "test");
-//   const docSnap = await getDoc(docRef);
-//   console.log("docSnap", docSnap);
-//   return docSnap.data();
+// const FIREBASE_COLLECTION_NAME = "findpicLists";
+
+// export const fetchFindpicData = async () => {
+//   try {
+//     const docRef = doc(db, FIREBASE_COLLECTION_NAME);
+//     const docSnap = await getDoc(docRef);
+
+//     if (docSnap.exists()) {
+//       console.log("data", docSnap.data());
+//       return docSnap.data();
+//     } else {
+//       console.log("해당 문서가 존재하지 않습니다!");
+//       return null; // 또는 다른 적절한 처리를 수행하세요.
+//     }
+//   } catch (error) {
+//     console.error("문서를 가져오는 중 오류 발생:", error);
+//     throw error; // 오류를 호출자에게 전파할 수도 있습니다.
+//   }
 // };
-// // console.log(fetchData());
-// console.log("db", db);
