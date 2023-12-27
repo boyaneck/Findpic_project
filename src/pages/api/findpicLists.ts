838424ed4ff4217d2d31f818a66e72d692b7690b
Hamin Lee <whereanna7@gmail.com>
2023-12-27 14:32:03 +0900

    [add] api findpicLists file, common file

---
+++ src/pages/api/findpicLists.ts
@@ -0,0 +1,12 @@
+// import { useEffect } from "react";
+// import { Firestore, doc, getDoc } from "firebase/firestore";
+// import { db } from "@/common/firebase";
+
+// const fetchData = async () => {
+//   const docRef = doc(db, "test");
+//   const docSnap = await getDoc(docRef);
+//   console.log("docSnap", docSnap);
+//   return docSnap.data();
+// };
+// // console.log(fetchData());
+// console.log("db", db);
